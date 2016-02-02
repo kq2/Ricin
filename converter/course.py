290befca1c719277cc5071575cf80e6fc8618a49
kq2 <kq2@rice.edu>
2016-02-01 21:01:02 -0600

    Add converter package.

--- converter/course.py
+++ converter/course.py
@@ -5,11 +5,17 @@ from scraper import util
 
 
 class Course:
-    def __init__(self, path):
-        self.path = path
-        self.folder = util.make_folder('../%s-canvas/' % path)
+    def __init__(self, coursera_folder):
+        self.coursera_folder = coursera_folder
+        self.canvas_folder = util.make_folder('../%s-canvas/' % coursera_folder)
 
-    def get_path(self):
-        return self.path
+    def get_coursera_folder(self):
+        return self.coursera_folder
+
+    def get_canvas_folder(self):
+        return self.canvas_folder
+
+    def convert_quizzes(self):
+        pass
 
 course = Course('interactivepython1-008')
---
+++ converter/quiz.py
@@ -0,0 +1,8 @@
+"""
+Convert a Coursera quiz into a Canvas quiz.
+"""
+
+
+def convert():
+    pass
+
